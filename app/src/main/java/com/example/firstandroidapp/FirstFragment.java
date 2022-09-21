6909c42406f54da41ed5032febb30f606ac13ba3
ZzCry12 <dariorata@gmail.com>
2022-09-21 23:03:31 +0200

    v0.2

--- app/src/main/java/com/example/firstandroidapp/FirstFragment.java
+++ app/src/main/java/com/example/firstandroidapp/FirstFragment.java
@@ -2,6 +2,9 @@ package com.example.firstandroidapp;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -9,6 +12,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -44,6 +48,11 @@ public class FirstFragment extends Fragment {
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
